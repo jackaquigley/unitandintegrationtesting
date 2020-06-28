ad5bc3b4023c959835432404114cdd55fb55bec4
Jack Quigley <jackaquigley@gmail.com>
2020-06-28 23:25:33 +0100

    integration testing now works for calculations

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -26,4 +26,14 @@ describe('calculator functionality', function() {
   expect(total.getAttribute('value')).to.eventually.equal('7')
   });
 
+  // the calculator should return the result of calcaulations
+  it('should update the calculator with the total of a calculation', function(){
+  let total = element(by.css('#running_total'))
+  element(by.css('#number4')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number4')).click();
+  element(by.css('#operator_equals')).click();
+  expect(total.getAttribute('value')).to.eventually.equal('1')
 });
+
+})
