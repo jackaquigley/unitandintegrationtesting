76cd10f94f020639cce278a7cdfd61fad9e7b675
Jack Quigley <jackaquigley@gmail.com>
2020-06-28 23:33:12 +0100

    divide by 0 test passes"

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -48,4 +48,27 @@ describe('calculator functionality', function() {
   expect(total.getAttribute('value')).to.eventually.equal('-3')
   })
 
+  //the calculator can handle large numbers
+  it('should be able to handle large numbers', function(){
+  let total = element(by.css('#running_total'))
+  element(by.css('#number4')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number1')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#operator_equals')).click();
+  expect(total.getAttribute('value')).to.eventually.equal('4000')
+  })
+
+  //the calculator should handle dividing by 0
+  it('should be able to handle dividing by 0', function(){
+  let total = element(by.css('#running_total'))
+  element(by.css('#number4')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#operator_equals')).click();
+  expect(total.getAttribute('value')).to.eventually.equal('Infinity')
+})
+
 })
